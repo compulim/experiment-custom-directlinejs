1c1888e2ba0b4e0a3755e2ce539202ced39d406d
William Wong <compulim@users.noreply.github.com>
2023-12-04 21:12:03 +0000

    Add log

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -5,6 +5,7 @@ import ReactWebChat from 'botframework-webchat';
 import FakeNetworkInformation from '../util/FakeNetworkInformation';
 
 const App = () => {
+  const [log, setLog] = useState<readonly string[]>(Object.freeze([]));
   const [token, setToken] = useState<string>('');
 
   const networkInformation = useMemo(() => new FakeNetworkInformation(), []);
@@ -52,24 +53,45 @@ const App = () => {
     return () => abortController.abort();
   }, [setToken]);
 
+  useEffect(() => {
+    if (!directLine) {
+      return;
+    }
+
+    const subscription = directLine.connectionStatus$.subscribe(value => {
+      setLog(log =>
+        Object.freeze([...log, `${new Date().toLocaleTimeString()}: connectionStatus changed to ${value}.`])
+      );
+    });
+
+    return () => subscription.unsubscribe();
+  }, [directLine, setLog]);
+
   return (
     <main>
-      <div className="network-bar">
-        <div className="network-bar__buttons">
-          <button disabled={type === 'none'} onClick={handleOfflineClick} type="button">
-            Offline
-          </button>
-          <button disabled={type !== 'none'} onClick={handleOnlineClick} type="button">
-            Online
-          </button>
-          <button disabled={type === 'none'} onClick={handleToggleClick} type="button">
-            Toggle type
-          </button>
+      <nav className="nav-bar">
+        <div className="box network-bar">
+          <div className="box__title">Control</div>
+          <div className="network-bar__buttons">
+            <button disabled={type === 'none'} onClick={handleOfflineClick} type="button">
+              Offline
+            </button>
+            <button disabled={type !== 'none'} onClick={handleOnlineClick} type="button">
+              Online
+            </button>
+            <button disabled={type === 'none'} onClick={handleToggleClick} type="button">
+              Toggle type
+            </button>
+          </div>
+          <div className="network-bar__type">
+            <code>NetworkInformation.type === "{type}"</code>
+          </div>
         </div>
-        <div className="network-bar__type">
-          <code>NetworkInformation.type === "{type}"</code>
-        </div>
-      </div>
+        <pre className="box log" role="log">
+          <div className="box__title">Log</div>
+          {log.join('\n')}
+        </pre>
+      </nav>
       {directLine && <ReactWebChat className="web-chat" directLine={directLine} />}
     </main>
   );
